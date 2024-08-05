806ab202f3c453ce0435355b22d69e1af841de46
Areeb <areeb@nayantech.com>
2024-08-05 16:39:55 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  resources :comments
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+    
+  devise_for :users
+
+
+  resources :users, only: [:show]
+
+  get 'home/about'
+  get 'posts/myposts'
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+   root "posts#index"
+end
